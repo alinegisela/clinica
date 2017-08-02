1a1d04054ee8e4916ba029ed1ed9bb45e5e14a02
jlcarvalho <lucianoalves413@gmail.com>
2017-08-02 19:46:23 -0300

    Criação do metodo Cadastrar Pacote

--- controle.py
+++ controle.py
@@ -1,6 +1,7 @@
 #Criação da classe CONTROLE
 from cliente import Cliente
 from pacote import Pacote
+from Tratamento import Tratamento
 
 class Controle:
 
@@ -12,9 +13,17 @@ class Controle:
         self.listaVendas = []
         self.listaPacotes = []
 
+    def cadastrar_tratamento(self, nome, valor):
+
+        novo_tratamento = Tratamento(nome, valor)
+        self.listaTratamentos.append(novo_tratamento)
+
+    def cadastrar_pacote(self, lista_Tramento):
+        self.listaPacotes.append(lista_Tramento)
+
     def cadastrar_cliente(self, nome, cpf, endereco, telefone, email):
 
         novo_cliente = Cliente(nome, cpf, endereco, telefone, email)
 
         self.listaClientes.append(novo_cliente)
-        print 'adicionado com sucesso'
+        
