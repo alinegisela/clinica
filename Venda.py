7789595e368e1a4a02a5362040366715a3a73f47
jlcarvalho <lucianoalves413@gmail.com>
2017-08-01 23:07:08 -0300

    Criação da classe Venda

---
+++ Venda.py
@@ -0,0 +1,13 @@
+#Classe Venda
+from cliente import Cliente
+from pacote import Pacote
+
+class Venda:
+
+    def __init__(self, cliente, pacote, data, valorTotal):
+        self.cliente  = cliente
+        self.pacote = pacote
+        self.data = date
+        self.valorTotal = valorTotal
+
+    
