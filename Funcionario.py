25e77274352c9ea1b55262f0ca354104ce63860d
alinegisela <aline.rlt@hotmail.com>
2017-08-02 06:48:59 -0300

    Add files via upload

---
+++ Funcionario.py
@@ -0,0 +1,12 @@
+class Funcionario:
+	
+	def __init__(self, nome, cpf, endereco, telefone, dt_nasc, email, cargo, salario):
+	
+                self.nome = nome
+		self.cpf = cpf
+		self.endereco = endereco
+		self.telefone = telefone
+		self.dt_nasc = dt_nasc
+		self.email = email
+		self.cargo = cargo
+		self.salario = salario
