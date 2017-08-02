c2bb64468d9d7b1a672fcf6abe56c2c201081e0a
jlcarvalho <lucianoalves413@gmail.com>
2017-08-01 22:34:25 -0300

    Criação da classe Cliente

---
+++ Cliente.py
@@ -0,0 +1,13 @@
+# Classe Cliente
+
+class Cliente:
+
+    def __init__(self, nome, cpf, endereco, telefone, email):
+
+        self.nome = nome
+        self.cpf = cpf
+        self.endereco = endereco
+        self.telefone = telefone
+        self.email = email
+
+    
