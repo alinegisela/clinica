79ccb7a3cdfb7b2bd07ca7e3c18f0f7bb44c027a
alinegisela <aline.rlt@hotmail.com>
2017-08-07 22:38:35 -0300

    correção no import

--- Venda.py
+++ Venda.py
@@ -1,13 +1,13 @@
 #Classe Venda
-from cliente import Cliente
-from pacote import Pacote
+from Cliente import Cliente
+from Pacote import Pacote
 
 class Venda:
 
     def __init__(self, cliente, pacote, data, valorTotal):
         self.cliente  = cliente
         self.pacote = pacote
-        self.data = date
+        self.data = data
         self.valorTotal = valorTotal
 
-    
+   
