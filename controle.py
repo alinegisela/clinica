87ce265be25f5ce6e4e2e8fdb9d90224314956e6
alinegisela <aline.rlt@hotmail.com>
2017-08-07 23:02:49 -0300

    Função listar lucro por mês

--- controle.py
+++ controle.py
@@ -1,7 +1,9 @@
 #Criacao da classe CONTROLE
 from Cliente import Cliente
 from Pacote import Pacote
+from Venda import Venda
 from Tratamento import Tratamento
+from datetime import date
 
 class Controle:
 
@@ -10,7 +12,7 @@ class Controle:
         self.listaClientes = []
         self.listaFuncionarios = []
         self.listaTratamentos = []
-        self.listaVendas = []
+        self.listaVendas = [ Venda(Cliente("aline", "123", "rua tal", "87", "aline@"), Pacote(200, Tratamento("tratamento 1 ", 200)), date(2017,3,15), 800), Venda(Cliente("aline", "123", "rua tal", "87", "aline@"), Pacote(200, Tratamento("tratamento 1 ", 200)), date(2017,1,15), 1000)]
         self.listaPacotes = []
 
     def cadastrar_tratamento(self, nome, valor):
@@ -37,3 +39,43 @@ class Controle:
     
         return pacotes
 
+    #retorna uma lista com os lucros por mes, a partir do mes da primeira venda
+    def listar_lucro_mes(self):
+        
+        vendas = sorted(self.listaVendas, key=lambda venda: venda.data)
+        mes_anterior = 0
+        ano_anterior = 0
+        lucro_mes = 0
+        lucros = []
+
+        for i in range(len(vendas)):
+            mes_atual = vendas[i].data.month
+            ano_atual = vendas[i].data.year
+            if i == 0:
+                
+                mes_anterior = mes_atual
+                ano_anterior = ano_atual
+                lucro_mes += vendas[i].valorTotal * 0.6
+                
+            elif ano_atual == ano_anterior and mes_atual == mes_anterior:
+                lucro_mes += vendas[i].valorTotal * 0.6
+                
+            else:
+                
+                lucros.append(lucro_mes)
+                lucro_mes = 0
+
+                diferenca_ano = ano_atual - ano_anterior
+                diferenca_mes = mes_atual - mes_anterior
+
+                for j in range((12 * diferenca_ano + diferenca_mes)-1):
+                    lucros.append(0)
+                
+                ano_anterior = vendas[i].data.year
+                mes_anterior = vendas[i].data.month
+                lucro_mes = vendas[i].valorTotal * 0.6
+                
+        lucros.append(lucro_mes)
+        return lucros        
+            
+        
